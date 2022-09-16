2958d7a25bec013abb3f08c8ea889332fad3aa59
theresa parmar <109839668+theresaparmar12@users.noreply.github.com>
2022-09-16 19:38:21 +0530

    Add files via upload

---
+++ 02-day-passargumenttofunction.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract FunctionArgument{
+uint num =10;
+ 
+ function set(uint _item) public {
+  num=_item;
+ }
+ function get() public view returns(uint){
+ return num;
+ }
+}
