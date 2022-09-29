baa8ab32b9e467e77f73ef308b737aaabc4756cb
theresa parmar <109839668+theresaparmar12@users.noreply.github.com>
2022-09-29 18:34:40 +0530

    Add files via upload

---
+++ 15-day-arraytoeven.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Day2 {
+function even(uint256[] memory arr, uint256 len)                                           
+        public
+        pure
+        returns (uint256[] memory)
+    {
+        for (uint256 i = 0; i < len; i++) {
+            arr[i] = 2 * arr[i];
+        }
+        return arr;
+    }
+}
